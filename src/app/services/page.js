3cbc3317d6ab584bdefbf09bbcd7298fdd875158
kohcharles <charles@creatual.io>
2023-12-04 19:11:00 +0800

    sizth

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,4 +1,7 @@
 
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
 export default function services() {
   return (
     <main className="min-h-screen items-center">
