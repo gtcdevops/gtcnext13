82b54bc1ce46e885b50f93be75fc78dde50e72ba
kohcharles <charles@creatual.io>
2023-12-14 16:23:58 +0800

    remove test folder

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  
-}
+const config = {
+    experimental: {
+      serverActions: true,
+    },
+  }
 
 module.exports = nextConfig
--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -4,44 +4,7 @@ import BookingSummary from "../components/BookingSummary"
 
 
 
-// import { generateServerClientUsingCookies } from '@aws-amplify/adapter-nextjs/api';
-// import { cookies } from 'next/headers';
 
-// import { revalidatePath } from 'next/cache';
-// import * as mutations from '../../graphql/mutations';
-
-// import config from '../../amplifyconfiguration.json';
-
-// const cookiesClient = generateServerClientUsingCookies({
-//   config, 
-//   cookies});
-
-// async function createTodo(formData) {
-//   "use server"
-//   const { data } = await cookiesClient.graphql({
-//     query: mutations.createTodo,
-//     variables: {
-//       input: {
-//         name: formData.get('name')?.toString() ?? '',
-//         name: formData.get('email')?.toString() ?? '',
-//         name: formData.get('contactno')?.toString() ?? '',
-//         name: formData.get('pax')?.toString() ?? '',
-//         name: formData.get('luggage')?.toString() ?? '',
-//         name: formData.get('typeoftransfer')?.toString() ?? '',
-//         name: formData.get('pickup')?.toString() ?? '',
-//         name: formData.get('dropoff')?.toString() ?? '',
-//         name: formData.get('date')?.toString() ?? '',
-//         name: formData.get('flightno')?.toString() ?? '',
-//         name: formData.get('pickuptime')?.toString() ?? '',
-//         name: formData.get('postal')?.toString() ?? '',
-//         name: formData.get('fare')?.toString() ?? '',
-//         name: formData.get('typeofvehicle')?.toString() ?? ''
-//       }
-//     }
-//   });
-//   console.log("Created Todo: ", data?.createTodo )
-//   revalidatePath('/');
-// }
 
 const Page = () => {
   return (
